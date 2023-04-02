84a315fea3e3b5dd46e52a49a78a94087aa87d6c
CAMARM_Flipz <armand.camponovo@icloud.com>
2023-04-02 18:43:26 +0200

    first commit

---
+++ components/Navbar.vue
@@ -0,0 +1,15 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div class="nav">
+    <div class="item logo"></div>
+    <div class="item">About</div>
+    <div class="item">Work</div>
+    <div class="item">Contact</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
