9a9c8a33af2a83e79a5f283845919d00e3766cb3
camarm <armand@camponovo.xyz>
2023-07-23 14:57:58 +0200

    [Add] opengraph meta tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,9 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
+      { name: 'og:name', content: 'camarm, 15yo french developer and growing sysadmin.' },
+      { name: 'og:image', content: 'https://github.com/camarm-dev/www.camarm.dev/raw/master/.github/header.png' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
