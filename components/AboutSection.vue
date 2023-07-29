cdfd059d13f16efa6688a75c9aaf5856de51c9ce
camarm <armand@camponovo.xyz>
2023-07-29 19:51:05 +0200

    [Fix] Prettifying

--- components/AboutSection.vue
+++ components/AboutSection.vue
@@ -36,9 +36,12 @@ export default defineComponent({
       <div class="content">
         <h2>My engagements</h2>
         <p>
-          I'm always trying to use open source and privacy respectives tools. That’s why I host alternatives to GAFAM's services on my server.
-          I use Linux (Ubuntu) 90% of the time to code and work and only use windows to play games. Linux is today a good alternative to windows, it’s free, open source and powerful !
-          I can't stand data privacy violation made particularly by the GAFAM. That's why I support open source and open projects !
+          I'm always trying to use open source and privacy respectives tools. That’s why I host alternatives to GAFAM's
+          services on my server.
+          I use Linux (Ubuntu) 90% of the time to code and work and only use windows to play games. Linux is today a
+          good alternative to windows, it’s free, open source and powerful !
+          I can't stand data privacy violation made particularly by the GAFAM. That's why I support open source and open
+          projects !
         </p>
       </div>
       <div class="media pink">
@@ -52,13 +55,17 @@ export default defineComponent({
       <div class="content">
         <h2>Skills</h2>
         <p>
-          Since I’ve started coding, I made a lot of web applications and PWA, with dynamic javascript and a Python backend in Flask.
-          As database, I mostly use Mongodb and I can't separate it. By working on Linux, I know this OS and the use of the shell.
+          Since I’ve started coding, I made a lot of web applications and PWA, with dynamic javascript and a Python
+          backend in Flask.
+          As database, I mostly use Mongodb and I can't separate it. By working on Linux, I know this OS and the use of
+          the shell.
           With my server I learnt how to properly deploy and maintain applications and their load management.
           I'm currently learning nuxt.js and Rust.
-          You can check <NuxtLink to="/work">
+          You can check
+          <NuxtLink to="/work">
             my projects
-          </NuxtLink> to see me in action.
+          </NuxtLink>
+          to see me in action.
         </p>
       </div>
     </article>
@@ -67,7 +74,8 @@ export default defineComponent({
         <h2>Experiences</h2>
         <p>
           Last year I made a portfolio for Ondine CAMPONOVO. It was my first experience with a client.
-          I did a training period at a hardware maintenance company where I learnt a lot of things on hardware and clients relations.
+          I did a training period at a hardware maintenance company where I learnt a lot of things on hardware and
+          clients relations.
           It was my first experience in company. For 3 years now, I manage self-hosted services on my home server.
         </p>
       </div>
