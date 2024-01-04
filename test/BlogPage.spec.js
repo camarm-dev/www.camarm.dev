c650ccf67c732bbac84adb964850cfcfca3704cb
camarm <armand@camponovo.xyz>
2024-01-04 18:58:56 +0100

    [Fix] New test because new semantic

--- test/BlogPage.spec.js
+++ test/BlogPage.spec.js
@@ -1,17 +1,12 @@
-import { describe, it, expect } from 'vitest'
-import { mount } from '@vue/test-utils'
+import {describe, it, expect} from 'vitest'
+import {mount} from '@vue/test-utils'
 import BlogPage from '@/pages/blog/index.vue'
+
 describe('Blog page', () => {
-  it('shows all the blog articles', async () => {
-    const articles = await fetch('https://dev.to/api/articles?username=camarm').then(resp => resp.json())
-    const page = mount(BlogPage, {
-      data () {
-        return {
-          articles
-        }
-      }
+    it('shows all the blog articles', async () => {
+        const articles = await fetch('https://dev.to/api/articles?username=camarm').then(resp => resp.json())
+        const page = mount(BlogPage)
+        await page.setData({articles: articles})
+        expect(page.vm.$data.articles.length).toBe(articles.length)
     })
-    const articlesElements = page.findAll('.article')
-    expect(articlesElements.length).toBe(articles.length)
-  })
 })
